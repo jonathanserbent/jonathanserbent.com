fa98d78565a70196f9927582468fc1157e92d3e1
Jonathan Serbent <jonathanserbent@gmail.com>
2021-01-04 10:52:11 -0600

    Update Resume
    Updated Resume and Added Dates

---
+++ src/app/Dates.tsx
@@ -0,0 +1,6 @@
+
+export const Dates = {
+    resume: "January 4th 2021",
+    website: "January 4th 2021",
+    copyright: "2020-2021"
+}
--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -8,22 +8,40 @@ import { default as ResumeEducationData } from "../data/Resume/ResumeEducation.j
 import { default as ResumeSkillsData } from "../data/Resume/ResumeSkills.json";
 import { default as ResumeExperienceData } from "../data/Resume/ResumeExperience.json";
 import { default as ProjectsData } from "../data/Projects/Projects.json";
+import { Dates } from "../app/Dates";
 
 const Resume = () => (
-    <Container className="bg-white text-dark my-5 rounded">
-        <Row>
-            <Col md={4} className="mx-0 px-0">
-                <ResumeEducation {...ResumeEducationData}/>
-                <ResumeCoursework {...ResumeEducationData}/>
-                <ResumeSkills {...ResumeSkillsData}/>
-            </Col>
-            <Col md={8} className="mx-0 px-0">
-                <ResumeExperience {...ResumeExperienceData}/>
-                <ResumeProjects {...ProjectsData} />
+    <Container fluid>
+        <Row className="justify-content-center mt-3">
+            <Col lg={6}>
+                <h3 className="text-center">My Resume</h3>
+                <p>This section of my website is here so that recruiters can see a virtual version of my resume. Since this page may not always be completely up to date, it does not represent an official version of my resume. However, I will try to always keep this page up to date, and you can check when it was last updated below. </p>
             </Col>
         </Row>
-        
+        <Row>
+            <Col>
+                <h3 className="text-center">Last Updated</h3>
+                <h4 className="text-center">{Dates.resume}</h4>
+            </Col>
+        </Row>
+        <Container className="bg-white text-dark my-5 rounded">
+            <h1 className="display-3 text-center">Jonathan Serbent</h1>
+            <hr/>
+            <Row>
+                <Col md={4} className="mx-0 px-0">
+                    <ResumeEducation {...ResumeEducationData}/>
+                    <ResumeCoursework {...ResumeEducationData}/>
+                    <ResumeSkills {...ResumeSkillsData}/>
+                </Col>
+                <Col md={8} className="mx-0 px-0">
+                    <ResumeExperience {...ResumeExperienceData}/>
+                    <ResumeProjects {...ProjectsData} />
+                </Col>
+            </Row>
+            
+        </Container>
     </Container>
+    
     
 );
 
